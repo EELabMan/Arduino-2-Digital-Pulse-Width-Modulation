a86ba022ab6a22dfc93caef0001d63a0d903fca1
EELabMan <91903583+EELabMan@users.noreply.github.com>
2022-06-01 08:51:02 -0500

    Update led_button.ino

--- led_button/led_button.ino
+++ led_button/led_button.ino
@@ -2,18 +2,18 @@
 Code for Hall Effect Sensor
 */
 
-const int LED=13;      //The LED is connected to pin 9
-const int HallEffect=2;      //The Button is connected to pin 2
+const int LED=9;      //The LED is connected to pin 9
+const int BUTTON=2;      //The Button is connected to pin 2
 
 void setup()
 {
   pinMode (LED, OUTPUT);     //Set the LED pin as an output
-  pinMode (HallEffect, INPUT);   //Set button as input (not required)
+  pinMode (BUTTON, INPUT);   //Set button as input (not required)
 }
 
 void loop()
 {
-  if (digitalRead(HallEffect) == HIGH)
+  if (digitalRead(BUTTON) == LOW)
   {
     digitalWrite(LED, LOW);
   }
